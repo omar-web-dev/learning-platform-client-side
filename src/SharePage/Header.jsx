c438de8efd43798ca8a1a90ffdda0fe08343aab0
omarwebdeve <omarf977865@gmail.com>
2022-10-25 20:02:54 +0600

    header responsive

--- src/SharePage/Header.jsx
+++ src/SharePage/Header.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className="absolute opacity-100 z-50 text-black bg-white w-full flex justify-between items-center mx-auto px-8 h-20">
-            <div className='flex justify-between'>
-                <div className="inline-flex items-center pr-5">
+        <div className='px-2 lg:px-[10%] bg-gray-100'>
+            <div className="navbar px-0 block  bg-gray-100">
+                <div className="lg:hidden flex justify-between">
+
+                    <div className="dropdown block">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-[290px] text-center">
+                            <li><NavLink className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/courses">
+                                <div className="flex items-center text-lg relative cursor-pointer whitespace-nowrap">Courses</div></NavLink></li>
+                            <li><NavLink className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/fqa">
+                                <div className="flex items-center relative text-lg cursor-pointer whitespace-nowrap">FQA</div></NavLink></li>
+                            <li><NavLink className="block md:inline-block py-2 px-3 text-lg hover:bg-yellow-700 rounded-md" to="/blog">
+                                <div className="flex items-center relative cursor-pointer whitespace-nowrap">Blog</div></NavLink></li>
+                            <li><NavLink className="block md:inline-block py-2 px-3 text-lg  hover:bg-yellow-700 rounded-md" to="/sign-in">
+                                <div className="flex items-center relative cursor-pointer whitespace-nowrap">Sign In</div></NavLink></li>
+                        </ul>
+                    </div>
                     <Link className="" to="/" >
-                    <img className='w-32' src="https://i.ibb.co/vhxyr49/Innovation-Hub-Logo-rgb.png" alt="Innovation-Hub-Logo" />
+                        <img className='w-32' src="https://i.ibb.co/vhxyr49/Innovation-Hub-Logo-rgb.png" alt="Innovation-Hub-Logo" />
                     </Link>
                 </div>
-            </div>
-
-            <div className="flex-initial">
-                <div className="flex justify-end items-center relative">
-                    <div className="block md:flex mr-4 items-center">
-                        
-                        <Link className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/courses">
-                            <div className="flex items-center text-lg relative cursor-pointer whitespace-nowrap">Courses</div>
-                        </Link>
-                        <Link className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/fqa">
-                            <div className="flex items-center relative text-lg cursor-pointer whitespace-nowrap">FQA</div>
-                        </Link>
-                        <Link className="block md:inline-block py-2 px-3 text-lg hover:bg-yellow-700 rounded-md" to="/blog">
-                            <div className="flex items-center relative cursor-pointer whitespace-nowrap">Blog</div>
-                        </Link>
-                        <Link className="block md:inline-block py-2 px-3 text-lg  hover:bg-yellow-700 rounded-md" to="/log-in">
-                            <div className="flex items-center relative cursor-pointer whitespace-nowrap">LogIn</div>
-                        </Link>
-                    </div>
+                <div className="navbar-center hidden lg:flex justify-between">
+                    <Link className="" to="/" >
+                        <img className='w-32' src="https://i.ibb.co/vhxyr49/Innovation-Hub-Logo-rgb.png" alt="Innovation-Hub-Logo" />
+                    </Link>
+                    <ul className="menu menu-horizontal p-0">
+                        <li><NavLink className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/courses">
+                            <div className="flex items-center text-lg relative cursor-pointer whitespace-nowrap">Courses</div></NavLink></li>
+                        <li><NavLink className="block md:inline-block py-2 px-3 hover:bg-yellow-700 rounded-md" to="/fqa">
+                            <div className="flex items-center relative text-lg cursor-pointer whitespace-nowrap">FQA</div></NavLink></li>
+                        <li><NavLink className="block md:inline-block py-2 px-3 text-lg hover:bg-yellow-700 rounded-md" to="/blog">
+                            <div className="flex items-center relative cursor-pointer whitespace-nowrap">Blog</div></NavLink></li>
+                        <li><NavLink className="block md:inline-block py-2 px-3 text-lg  hover:bg-yellow-700 rounded-md" to="/sign-in">
+                            <div className="flex items-center relative cursor-pointer whitespace-nowrap">Sign In</div></NavLink></li>
+                    </ul>
                 </div>
             </div>
-        </nav>
+        </div>
     );
 };
 
